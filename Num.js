baae47042091b356674ac870564473f321afd41c
Nirmal-Singh-G <nirmalsinghg.10@gmail.com>
2026-02-10 18:42:48 +0530

    Nirmal Project

---
+++ Num.js
@@ -0,0 +1,13 @@
+function findLargest(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+}
+
+console.log(findLargest([10, 5, 22, 8]));
--- String.js
+++ String.js
@@ -11,3 +11,17 @@ function removeDuplicates(arr) {
 }
 
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4]));
+
+function findLargest(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+}
+
+console.log(findLargest([10, 5, 22, 8]));
